e0e6acd81d8144ffe23fa76d439f9d3243335bcb
Ragava2002 <sec19ec124@sairamtap.edu.in>
2023-07-03 15:33:24 +0000

    commit

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
+import './components/Sidebar/Sidebar.css';
 import Login from './components/login';
 import SignUp from './components/signup';
 import ForgotPassword from './components/forgotpassword';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import Sidebar from './components/Sidebar/Sidebar';
+import Home from './pages/Home.js';
+import Projects from './pages/Projects.js';
+import MyTasks from './pages/MyTasks.js';
+import Profile from './pages/Profile';
+import Chat from './pages/Chat.js';
+import Logout from './pages/Logout.js';
 import ListProjects from './pages/listProjects';
 
 
@@ -12,6 +20,7 @@ function App() {
   return (
     <div>
       <Router>
+      <Sidebar>
       <ToastContainer/>
         <Routes>
          
@@ -19,7 +28,16 @@ function App() {
           <Route path="/signup" element={<SignUp />} />
           <Route path="/forgotpassword" element={<ForgotPassword />} />
           <Route path="/listprojects" element={<ListProjects />} />
+          <Route path="/" element={<Home />  } />
+          <Route path="/home" element={ <Home />} />
+          <Route path="/projects" element={<Projects />} /> 
+          <Route path="/mytasks" element={<MyTasks />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/chat" element={<Chat />} />
+          <Route path="/logout" element={<Logout />} />
+
         </Routes>
+      </Sidebar>
       </Router>
     </div>
   );
