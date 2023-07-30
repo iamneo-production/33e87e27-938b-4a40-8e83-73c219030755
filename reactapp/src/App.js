312e9bb0ea748e2ed6012ecdc58a39cba8560860
Vinith Kumar <60223365+vinith4@users.noreply.github.com>
2023-07-30 12:40:00 +0530

    Error changes

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -2,13 +2,7 @@ import './App.css';
 import { BrowserRouter } from 'react-router-dom';
 import { ContextProvider } from './context';
 import AppRoute from './router/Approute';
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-// import './style.scss';
->>>>>>> Project-Workspace-anjanisree18
-=======
->>>>>>> afd7048be635e9bbb36f5f4adf3cc0b5b122f5d3
+
 
 function App() {
   return (
